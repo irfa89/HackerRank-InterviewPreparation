aa21e7141a3f5cfbed64c9c24bad1b5da942c680
Shariq Khan <33172597+irfa89@users.noreply.github.com>
2018-09-25 01:38:51 -0400

    Second commit..

---
+++ ransomNote.py
@@ -0,0 +1,41 @@
+""""
+Complete the checkMagazine function in the editor below. It must print YES if the note can be formed using the magazine, or No.
+
+checkMagazine has the following parameters:
+magazine: an array of strings, each a word in the magazine
+note: an array of strings, each a word in the ransom note.
+"""
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    mag = list(magazine)
+    for r in note:
+        if r in mag:
+            mag.remove(r)
+        else:
+            return False
+    return True
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
+    if checkMagazine(magazine, note) == True:
+        print("Yes")
+    else:
+        print("No")
